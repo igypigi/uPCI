02bf69ddf29f1bc321804632039c4ef73f7b31a4
premik91 <i@premik91.com>
2014-06-01 09:11:01 +0200

    Added upci compute and graph draw.

--- uPCI.py
+++ uPCI.py
@@ -1,4 +1,8 @@
-from random import random
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+
+u = 1
 
 
 def read_file(file_name):
@@ -14,24 +18,38 @@ def read_file(file_name):
     return graph
 
 
-def uPCI(node):
-    # TODO: calculate uPCI
-    return int(random()*20)
+def compute_upci(graph, node):
+    neighbours_degree = []
+    for neighbour in graph[node]:
+        neighbours_degree.append(len(graph[neighbour]))
+    neighbours_degree.sort(reverse=True)
+
+    for neighbour_degree in neighbours_degree:
+        k = -0
+        e = +0
+        for d in neighbours_degree:
+            if d > neighbour_degree:
+                k += 1
+            elif d == neighbour_degree:
+                e += 1
+        if k <= u * neighbour_degree <= k + e:
+            return neighbour_degree
+    return -1
 
 
-def uPCI_for_every_node(graph):
-    uPCIs = {}
+def upci_for_every_node(graph):
+    upcis = {}
     for node in graph:
-        upci = uPCI(node)
-        if upci not in uPCIs:
-            uPCIs[upci] = []
-        uPCIs[upci].append(node)
-    return sorted(uPCIs.iteritems())
+        upci = compute_upci(graph, node)
+        if upci not in upcis:
+            upcis[upci] = []
+        upcis[upci].append(node)
+    return sorted(upcis.iteritems())
 
 
-def get_most_influential_nodes(graph, number_of_nodes=1):
+def get_most_influential_nodes_upci(graph, number_of_nodes=1):
     influential_nodes = []
-    for (upci, nodes) in uPCI_for_every_node(graph):
+    for (upci, nodes) in upci_for_every_node(graph):
         influential_nodes += nodes
         if len(influential_nodes) >= number_of_nodes:
             break
@@ -39,8 +57,20 @@ def get_most_influential_nodes(graph, number_of_nodes=1):
 
 
 def main():
-    graph = read_file("CA-CondMat.txt")
-    print get_most_influential_nodes(graph, 10)
+    graph = read_file("ca-AstroPh.txt")
+    #print get_most_influential_nodes_upci(graph, 10)
+    upci_nodes = upci_for_every_node(graph)
+    x = []
+    y = []
+    for (upci, nodes) in upci_nodes:
+        for node in nodes:
+            x.append(upci/2)
+            y.append(len(graph[node]))
+    plt.plot(x, y, 'ro')
+    plt.yscale('log')
+    plt.axis([0, 120, 0, 1000])
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
